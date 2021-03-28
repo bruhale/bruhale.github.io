9b3f391439323b44363dd73ace8be61d27ac66ef
bruhale <brucelhaleii@gmail.com>
2021-03-28 16:19:40 -0400

    My first commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+ 
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>We are the biggest supplier's of Electronics in Sweden.  From Drones to Electric Cars and Toys!</h1>
+        <h2>In Stock</h2>  
+                
+      </div>
+    );
+  }
+}
+ 
+export default Home;
---
+++ src/Main.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import ATV from "./ATV";
+import Drones from "./Drones";
+import ElectricCars from "./ElectricCars";
+import Toys from "./Toys";
+import Appliance from "./Appliance";
+ 
+class Main extends Component {
+    render() {
+      return (
+        <HashRouter>
+          <div>
+            <h1>ARNGENS.NET</h1>
+            <ul className="header">
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink to="/ATV">ATV</NavLink></li>
+            <li><NavLink to="/Drones">Drones</NavLink></li>
+            <li><NavLink to="/ElectricCars">Electric Cars</NavLink></li>
+            <li><NavLink to="/Toys">Toys</NavLink></li>
+            <li><NavLink to="/Appliance">Appliances</NavLink></li>
+            </ul>
+            <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/ATV" component={ATV}/>
+            <Route path="/Drones" component={Drones}/>
+            <Route path="/ElectricCars" component={ElectricCars}/>
+            <Route path="/Toys" component={Toys}/>
+            <Route path="/Appliance" component={Appliance}/>
+               
+            </div>
+          </div>
+          </HashRouter>
+      );
+    }
+  }
+   
+  export default Main;
